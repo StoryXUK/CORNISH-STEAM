c346a5d7fd74c15b9241f3845ced962b2ab68979
Responsibleuk <james@we-are-responsible.com>
2023-06-11 15:59:39 +0000

    Add

--- js/constellation.js
+++ js/constellation.js
@@ -1,230 +1,110 @@
-/*
- * requestAnimationFrame pollyfill
- */
-if (!window.requestAnimationFrame) {
-	window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (callback) {
-		return window.setTimeout(callback, 1000 / 60);
-	});
-}
 
-/*!
- * Mantis.js / jQuery / Zepto.js plugin for Constellation
- * @version 1.2.2
- * @author Acauã Montiel <contato@acauamontiel.com.br>
- * @license http://acaua.mit-license.org/
- */
-(function ($, window) {
-	/**
-	 * Makes a nice constellation on canvas
-	 * @constructor Constellation
-	 */
-	function Constellation (canvas, options) {
-		var $canvas = $(canvas),
-			context = canvas.getContext('2d'),
-			defaults = {
-				star: {
-					color: 'rgba(255, 255, 255, .9)',
-					width: 2
-				},
-				line: {
-					color: 'rgba(255, 255, 255, .9)',
-					width: 0.4
-				},
-				position: {
-					x: 0, // This value will be overwritten at startup
-					y: 0 // This value will be overwritten at startup
-				},
-				width: window.innerWidth,
-            	height: window.innerHeight,
-				velocity: 0.1,
-				length: 220,
-				distance: 120,
-				radius: 250,
-				stars: []
-			},
-			config = $.extend(true, {}, defaults, options);
+  particlesJS("hero-constellation", {
+    particles: {
+      number: {
+        value: 80, // Adjust the number of particles
+        density: {
+          enable: true,
+          value_area: 800 // Adjust the area density of particles
+        }
+      },
+      color: {
+        value: "#ffffff" // Adjust the color of particles
+      },
+      shape: {
+        type: "circle", // Adjust the shape of particles (e.g., circle, square, triangle, etc.)
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nb_sides: 5
+        },
+        image: {
+          src: "images/Miners_Dry_Hero.jpg", // Replace with your own particle image path
+          width: 100,
+          height: 100
+        }
+      },
+      opacity: {
+        value: 0.5, // Adjust the opacity of particles
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3, // Adjust the size of particles
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150, // Adjust the distance between particles
+        color: "#ffffff", // Adjust the color of the particle connections
+        opacity: 0.4, // Adjust the opacity of the particle connections
+        width: 1 // Adjust the width of the particle connections
+      },
+      move: {
+        enable: true,
+        speed: 6, // Adjust the speed of particles
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse" // Adjust the interaction mode on hover (e.g., grab, bubble, repulse, etc.)
+        },
+        onclick: {
+          enable: true,
+          mode: "push" // Adjust the interaction mode on click (e.g., push, remove, bubble, etc.)
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 100 // Adjust the repulsion distance on hover
+        },
+        push: {
+          particles_nb: 4 // Adjust the number of particles pushed on click
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
+  });
 
-		function Star () {
-			this.x = Math.random() * canvas.width;
-			this.y = Math.random() * canvas.height;
-
-			this.vx = (config.velocity - (Math.random() * 0.5));
-			this.vy = (config.velocity - (Math.random() * 0.5));
-
-			this.radius = Math.random() * config.star.width;
-		}
-
-		Star.prototype = {
-			create: function(){
-				context.beginPath();
-				context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-				context.fill();
-			},
-
-			animate: function(){
-				var i;
-				for (i = 0; i < config.length; i++) {
-
-					var star = config.stars[i];
-
-					if (star.y < 0 || star.y > canvas.height) {
-						star.vx = star.vx;
-						star.vy = - star.vy;
-					} else if (star.x < 0 || star.x > canvas.width) {
-						star.vx = - star.vx;
-						star.vy = star.vy;
-					}
-
-					star.x += star.vx;
-					star.y += star.vy;
-				}
-			},
-
-			line: function(){
-				var length = config.length,
-					iStar,
-					jStar,
-					i,
-					j;
-
-				for (i = 0; i < length; i++) {
-					for (j = 0; j < length; j++) {
-						iStar = config.stars[i];
-						jStar = config.stars[j];
-
-						if (
-							(iStar.x - jStar.x) < config.distance &&
-							(iStar.y - jStar.y) < config.distance &&
-							(iStar.x - jStar.x) > - config.distance &&
-							(iStar.y - jStar.y) > - config.distance
-						) {
-							if (
-								(iStar.x - config.position.x) < config.radius &&
-								(iStar.y - config.position.y) < config.radius &&
-								(iStar.x - config.position.x) > - config.radius &&
-								(iStar.y - config.position.y) > - config.radius
-							) {
-								context.beginPath();
-								context.moveTo(iStar.x, iStar.y);
-								context.lineTo(jStar.x, jStar.y);
-								context.stroke();
-								context.closePath();
-							}
-						}
-					}
-				}
-			}
-		};
-
-		this.createStars = function () {
-			var length = config.length,
-				star,
-				i;
-
-			context.clearRect(0, 0, canvas.width, canvas.height);
-
-			for (i = 0; i < length; i++) {
-				config.stars.push(new Star());
-				star = config.stars[i];
-
-				star.create();
-			}
-
-			star.line();
-			star.animate();
-		};
-
-		this.setCanvas = function () {
-			canvas.width = config.width;
-			canvas.height = config.height;
-		};
-
-		this.setContext = function () {
-			context.fillStyle = config.star.color;
-			context.strokeStyle = config.line.color;
-			context.lineWidth = config.line.width;
-		};
-
-		this.setInitialPosition = function () {
-			if (!options || !options.hasOwnProperty('position')) {
-				config.position = {
-					x: canvas.width * 0.5,
-					y: canvas.height * 0.5
-				};
-			}
-		};
-
-		this.loop = function (callback) {
-			callback();
-
-			window.requestAnimationFrame(function () {
-				this.loop(callback);
-			}.bind(this));
-		};
-
-		this.bind = function () {
-			$(window).on('mousemove', function(e){
-				config.position.x = e.pageX - $canvas.offset().left;
-				config.position.y = e.pageY - $canvas.offset().top;
-			});
-		};
-
-		this.init = function () {
-			this.setCanvas();
-			this.setContext();
-			this.setInitialPosition();
-			this.loop(this.createStars);
-			this.bind();
-		};
-	}
-
-	$.fn.constellation = function (options) {
-		return this.each(function () {
-			var c = new Constellation(this, options);
-			c.init();
-		});
-	};
-})($, window);
-
-function init_plugin() {
-
-   $('canvas').constellation({
-		star: {
-			color: 'rgba(255, 255, 255, .9)',
-			width: 2
-		},
-		line: {
-			color: 'rgba(255, 255, 255, .9)',
-			width: 0.3
-		},
-		position: {
-			x: 0, // This value will be overwritten at startup
-			y: 0 // This value will be overwritten at startup
-		},
-		velocity: 0.1,
-		length: 220,
-		distance: 120,
-		radius: 250,
-		stars: []
-   });
-}
-
-$(document).ready(function(){
-    
-    var onMobile = false;
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { onMobile = true; }
-    
-    if( ( onMobile === false ) ) {
-
-    	init_plugin(); //init plugin
-
-    	window.onresize = function() {
-		   cancelAnimationFrame(requestAnimationFrame); //cancel current animation frame
-		   $('canvas')[0].width = window.innerWidth; //reset canvas width
-		   $('canvas')[0].height = window.innerHeight; //reset canvas height
-		   init_plugin(); //init plugin
-		};
-        
-    } else {}
-    
-});
