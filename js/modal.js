2734e4cecd9c28b1b9515b1d59a98be8e3bfe23a
Responsibleuk <james@we-are-responsible.com>
2023-06-12 16:32:54 +0000

    Add

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,26 @@
-// Get the modal element and form iframe
-var modal = document.getElementById("modal");
-var formIframe = document.getElementById("form-iframe");
+Copy code
+// Get the modal element
+var modal = document.getElementById("myModal");
 
-// Get the modal button and close button
-var modalButton = document.getElementById("modal-button");
-var closeButton = document.getElementsByClassName("close")[0];
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
 
-// Set the form iframe source when the modal button is clicked
-modalButton.addEventListener("click", function() {
-  formIframe.src = "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAADHoRQNUOEJPTjQ4NzlXUkZIWUFLUjIzOUtWUkc1RS4u&embed=true";
-  modal.style.display = "block"; // Show the modal
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// Close the modal when the close button is clicked or outside the modal content is clicked
-closeButton.addEventListener("click", function() {
-  modal.style.display = "none"; // Hide the modal
-});
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
 
-window.addEventListener("click", function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none"; // Hide the modal
-  }
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
